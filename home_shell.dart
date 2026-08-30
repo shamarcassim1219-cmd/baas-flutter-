a01d4896bfba0a85ff8ba78e8dd5aa524d1a52ce
shamarcassim1219-cmd <shamarcassim1219@gmail.com>
2026-08-30 18:38:04 +0530

    Create home_shell.dart

---
+++ home_shell.dart
@@ -0,0 +1,9 @@
+// uda import ekak add karanna:
+    // import '../jobs/jobs_screen.dart';
+
+    const pages = [
+      JobsScreen(),
+      _Placeholder('Wallet'),
+      _Placeholder('Profile'),
+      _Placeholder('Notifications'),
+    ];
