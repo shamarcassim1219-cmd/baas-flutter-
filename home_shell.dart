246ec1e12b0c0a530cd3649080f757d2b4993693
shamarcassim1219-cmd <shamarcassim1219@gmail.com>
2026-08-30 18:42:24 +0530

    Delete home_shell.dart

--- home_shell.dart
+++
@@ -1,9 +0,0 @@
-// uda import ekak add karanna:
-    // import '../jobs/jobs_screen.dart';
-
-    const pages = [
-      JobsScreen(),
-      _Placeholder('Wallet'),
-      _Placeholder('Profile'),
-      _Placeholder('Notifications'),
-    ];
